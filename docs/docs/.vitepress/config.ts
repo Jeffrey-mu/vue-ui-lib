f481bbd3666e8d4b6f6056e7c3110d7513c73f71
Jeffrey <1115378579@qq.com>
2022-08-30 18:53:49 +0800

    wip: 添加首页 修改样式

--- docs/docs/.vitepress/config.ts
+++ docs/docs/.vitepress/config.ts
@@ -4,9 +4,7 @@ import { nav, sidebar } from './config/index'
 export default defineConfig({
   head: [
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }]
-    // would render: <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   ],
-  lang: 'en-US',
   title: 'vue-ui-libs',
   description: 'Vite & Vue powered static site generator.',
   lastUpdated: true,
@@ -20,10 +18,6 @@ export default defineConfig({
     nav,
     sidebar,
     lastUpdatedText: '最后更新时间',
-    docFooter: {
-      prev: '上一页',
-      next: '下一页'
-    },
     editLink: {
       pattern: 'https://github.com/Jeffrey-mu/vue-ui-lib/tree/master/docs/docs/:path',
       text: '在github上编辑此页面'
@@ -38,7 +32,5 @@ export default defineConfig({
       // 这里可以使用 markdown-it 插件，vitepress-theme-demoblock就是基于此开发的
       md.use(demoBlockPlugin)
     },
-    theme: 'material-palenight',
-    lineNumbers: true
   }
 })
---
+++ docs/docs/index1.md
@@ -0,0 +1,28 @@
+---
+layout: home
+
+hero:
+  name: vue-ui-libs
+  text: 是一个基于vue3.0 + ts 的ui库。
+  image:
+    # src: /Vue.png
+    alt: VitePress
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /guide/
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/Jeffrey-mu/vue-ui-lib
+features:
+  - icon: ⚡️
+    title: 组件丰富
+    details: 大量精美组件，持续更新……
+  - icon: 🖖
+    title: 实用指令
+    details: 封装很多实用指令，提示开发效率。
+  - icon: 🛠️
+    title: 公共函数
+    details: 封装多个函数,解决开发痛点。
+---
+
