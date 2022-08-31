b15f46932f36564291e1cde0cf0113ab7423d135
Jeffrey <1115378579@qq.com>
2022-08-31 19:14:53 +0800

    wip:晚上button单测

--- src/components/__tests__/Button.spec.ts
+++ src/components/__tests__/Button.spec.ts
@@ -1,16 +1,26 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import Button from '../Button/index.vue'
-describe('Button default slot', () => {
-  it('renders properly', () => {
+describe('render button', () => {
+  it('render button', () => {
     const wrapper = mount(Button, { props: { type: '1', subText: '123' } })
     expect(wrapper.text()).toContain('Read More')
   })
-})
-describe('Button slot', () => {
-  it('renders properly', () => {
+  it('slot is hello world', () => {
     // Button, { props, options: { slots: "hello button" } }
-    const wrapper = mount(Button, { props: { type: '1', subText: '123' }, slots: Button })
-    expect(wrapper.text()).toContain('Read More')
+    const wrapper = mount(Button, {
+      props: { type: '1', subText: '123' }, slots: {
+        default: "hello world"
+      }
+    })
+    expect(wrapper.text()).toContain('hello world')
+  })
+  it('test button event', () => {
+    // Button, { props, options: { slots: "hello button" } }
+    const wrapper = mount(Button)
+    wrapper.find('button').trigger('click')
+    const clickEvent = wrapper.emitted('click')
+    expect(wrapper.emitted()).toHaveProperty('click')
+    expect(clickEvent).toHaveLength(1)
   })
 })
