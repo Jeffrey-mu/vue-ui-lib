db4c2f3abd5a8108bccc9b86e7428eff87332dc6
Jeffrey <1115378579@qq.com>
2023-02-01 14:29:25 +0800

    commit

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,4 +3,11 @@ import { onMounted, ref } from "vue";
 import { confettiFunction } from "../index";
 onMounted(confettiFunction({ model: "fireworks" }));
 </script>
-<template></template>
+<template>
+  <ui-card dataImage="../../docs/docs/public/card.avif" :width="300" :height="590">
+    <template #header> Canyons </template>
+    <template #content>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    </template>
+  </ui-card>
+</template>
